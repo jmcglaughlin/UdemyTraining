3a904ae7621c48d9aa8bed47f347cc73a5bf30b1
John McGlaughlin <git@john-mcglaughlin.com>
2016-04-30 17:47:21 -0400

    save exercise 2 -2

--- 02_exercise/21.go
+++ 02_exercise/21.go
@@ -2,18 +2,14 @@ package main
 
 import "fmt"
 
-func div2(i int) (int, bool) {
-    return i /2, i % 2 == 0
-}
 
 func main() {
-    n, even := div2(1)
+    div2 := func (i int) (int, bool) {
+        return i /2, i % 2 == 0
+    }
     
-    fmt.Println(n, even)
-    
-    for ;;n++ {
+    for n:=1; n <= 10 ;n++ {
         fmt.Print(n, " ")
         fmt.Println(div2(n))
-        if n == 10 {break}
     }
 }
